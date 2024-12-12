a3415a261bf819e1aa8f44825cab321b954c2d4c
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-12 19:15:58 +0530

    Update Navbar.jsx

--- metube/src/Components/Navbar/Navbar.jsx
+++ metube/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Navbar.css'
+import menu_icon from '../../assets/menu.png'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <nav className='flex-div'>
-        <div className="nav-left flex-div"></div>
+        <div className="nav-left flex-div">
+            <img className='menu-icon' src={menu_icon} alt="" />
+            <img className='logo' src={logo} alt="" />
+        </div>
     </nav>
   )
 }
