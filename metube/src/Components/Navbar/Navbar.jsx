b2efef46bc962bc80bf423ce3ad0cf11b627b17d
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-12 19:17:12 +0530

    Update Navbar.jsx

--- metube/src/Components/Navbar/Navbar.jsx
+++ metube/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import menu_icon from '../../assets/menu.png'
 import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search.png'
 
 const Navbar = () => {
   return (
@@ -9,6 +10,11 @@ const Navbar = () => {
         <div className="nav-left flex-div">
             <img className='menu-icon' src={menu_icon} alt="" />
             <img className='logo' src={logo} alt="" />
+        </div>
+
+        <div className="nav-middle flex-div">
+            <input type="text" placeholder='Search' />
+            <img src={search_icon} alt="" />
         </div>
     </nav>
   )
