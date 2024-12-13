dc85adbbd9d1eeb0ff9988036daa79ea4448df84
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-13 21:31:38 +0530

    Update App.jsx

--- metube/src/App.jsx
+++ metube/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
     </div>
   )
 }
